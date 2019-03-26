752ecf31cec3a1f9f317c1417afc2400822d9681
ChtobiUchitsyaNadoBitProfi <ivan.mielnikov.01@mail.ru>
2019-03-26 16:01:38 +0700

    Add value entry functions

--- src/board_print_plain.c
+++ src/board_print_plain.c
@@ -4,6 +4,50 @@
 extern char board[8][8];
 extern int x1, y11, x2, y2;
 
+void scanh(int flag)
+{
+    char tempxy[6] = "NULL";
+    while (1) {
+        while (1) {
+            scanf("%s", tempxy);
+            if (strcmp(tempxy, "exit") == 0 || strcmp(tempxy, "0") == 0) {
+                game = -1;
+                return;
+            }
+            if (inter(tempxy)) {
+                break;
+            }
+            printf("Error,try gain:");
+        }
+        if (flag == 1) {
+            if (check1() == 1) {
+                break;
+            } else {
+                printf("Error, try again:");
+            }
+        }
+        if (flag == 2) {
+            if (check2() == 1) {
+                break;
+            } else
+                printf("Error, try again:");
+        }
+    }
+}
+
+int inter(char tempxy[6])
+{
+    x1 = (int)tempxy[1] - 49;
+    y11 = (int)tempxy[0] - 97;
+    x2 = (int)tempxy[4] - 49;
+    y2 = (int)tempxy[3] - 97;
+    if ((strlen(tempxy) == 5) && (x2 < 8) && (x2 >= 0) && (y2 >= 0) && (y2 < 8)
+        && (x1 >= 0) && (x1 < 8) && (y11 >= 0) && (y11 < 8)){
+        return 1;
+    }
+    return 0;
+}
+
 void showboard()
 {
     int i, j;
