3c58e46f8ba6a175bbc3fca85f971391b5d9b10c
ChtobiUchitsyaNadoBitProfi <ivan.mielnikov.01@mail.ru>
2019-04-23 11:09:54 +0700

    Add test/main.c

--- test/main.c
+++ test/main.c
@@ -1,36 +1,8 @@
-#include "board.h"
-#include "board_print_plain.h"
-#include <stdio.h>
-#include <string.h>
+#define CTEST_MAIN
 
-int game = 0;
-int x1, y11, x2, y2;
-char board[9][9] = {{'R', 'H', 'B', 'Q', 'K', 'B', 'H', 'R'},
-                    {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-                    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                    {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-                    {'r', 'h', 'b', 'k', 'q', 'b', 'h', 'r'}};
+#include "../thirdparty/ctest.h"
 
-int main()
+int main(int argc, const char** argv)
 {
-    showboard();
-    printf("Input example : a2-a3\n");
-    while (1) {
-        printf("player 1 : ");
-        scanh(1);
-        move();
-        printf("\033[2J");   /* Clear the entire screen. */
-        printf("\033[0;0f"); /* Move cursor to the top left hand corner */
-        showboard();
-        printf("player 2 : ");
-        scanh(2);
-        move();
-        printf("\033[2J");   /* Clear the entire screen. */
-        printf("\033[0;0f"); /* Move cursor to the top left hand corner */
-        showboard();
-    }
-    return 0;
-};
+    return ctest_main(argc, argv);
+}
